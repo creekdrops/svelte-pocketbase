3210919d03cb9acb19d35d6f27147df843f61714
CREEK <brandon@gridviewdesign.com>
2023-01-07 15:49:59 -0600

    pbStore is now a custom store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,4 +1,33 @@
-import type PocketBase from "pocketbase";
-import { writable, type Writable } from "svelte/store";
+import { browser } from '$app/environment';
+import PocketBase, { BaseAuthStore } from 'pocketbase';
+import { writable } from 'svelte/store';
 
-export const pbStore: Writable<PocketBase> = writable()
+function createPbStore(
+	baseUrl?: string | undefined,
+	authStore?: BaseAuthStore | null | undefined,
+	lang?: string | undefined
+) {
+	const store = writable<PocketBase>();
+
+	function set(
+		baseUrl?: string | undefined,
+		authStore?: BaseAuthStore | null | undefined,
+		lang?: string | undefined
+	) {
+		const pb = new PocketBase(baseUrl, authStore, lang);
+
+		if (browser) {
+			pb.authStore.loadFromCookie(document.cookie);
+		}
+		store.set(pb);
+	}
+
+	set(baseUrl, authStore, lang);
+
+	return {
+		subscribe: store.subscribe,
+		set
+	};
+}
+
+export const pbStore = createPbStore();
